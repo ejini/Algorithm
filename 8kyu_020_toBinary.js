1ba1229f638fb914a29407f6a396ee029de998b7
yej_h <yej8053@gmail.com>
2021-09-02 16:06:09 +0900

    020_2진 포맷 반환

---
+++ 8kyu_020_toBinary.js
@@ -0,0 +1,19 @@
+// 문제
+// 음수가 아닌 정수 n이 주어진다.  이 숫자를 2진 포맷(binary format)으로 반환한다.
+
+function isIsogram(str) {
+	// 풀이1
+	return n.toString(2);
+}
+
+toBinary(0) // 0
+toBinary(1) // 1
+toBinary(2) // 10
+toBinary(3) // 11
+toBinary(4) // 100
+toBinary(5) // 101
+toBinary(6) // 110
+toBinary(7) // 111
+toBinary(8) // 1000
+toBinary(9) // 1001
+toBinary(10) // 1010
