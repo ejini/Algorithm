20942919280dfc7a7ce0e813a5c14dfdc65536cc
yej_h <yej8053@gmail.com>
2021-10-01 21:33:27 +0900

    045_숫자를 단어로 반환

--- 8kyu_045_switchItUp.js
+++ 8kyu_045_switchItUp.js
@@ -1,48 +1,49 @@
-// 2021-09-02
+// 문제
 // 0-9 사이의 숫자가 제공되면, 단어로 변환한다.
 
 function switchItUp(n) {
-  // …
-  //1
-  // if (n === 0 ) return 'Zero'
-  // if (n === 1 ) return 'One'
-  // if (n === 2 ) return 'Two'
-  // if (n === 3 ) return 'Three'
-  // if (n === 4 ) return 'Four'
-  // if (n === 5 ) return 'Five'
-  // if (n === 6 ) return 'Six'
-  // if (n === 7 ) return 'Seven'
-  // if (n === 8 ) return 'Eightv'
-  // if (n === 9 ) return 'Nine'
+  
+  // 풀이1
+  if (n === 0 ) return 'Zero'
+  if (n === 1 ) return 'One'
+  if (n === 2 ) return 'Two'
+  if (n === 3 ) return 'Three'
+  if (n === 4 ) return 'Four'
+  if (n === 5 ) return 'Five'
+  if (n === 6 ) return 'Six'
+  if (n === 7 ) return 'Seven'
+  if (n === 8 ) return 'Eightv'
+  if (n === 9 ) return 'Nine'
 
-  // 송
-  // const obj = {
-  //   0: 'Zero',
-  //   1: 'One',
-  //   2: 'Two',
-  //   3: 'Three',
-  //   4: 'Four',
-  //   5: 'Five',
-  //   6: 'Six',
-  //   7: 'Seven',
-  //   8: 'Eight',
-  //   9: 'Nine',
-  // }
+  // 풀이2
+  const obj = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+  }
 
-  // return obj[n]
+  return obj[n]
 
+  // 풀이3
   const arr = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
 
   return arr[n]
 }
 
-console.log(switchItUp(0)); // Zero
-console.log(switchItUp(1)); // One
-console.log(switchItUp(2)); // Two
-console.log(switchItUp(3)); // Three
-console.log(switchItUp(4)); // Four
-console.log(switchItUp(5)); // Five
-console.log(switchItUp(6)); // Six
-console.log(switchItUp(7)); // Seven
-console.log(switchItUp(8)); // Eight
-console.log(switchItUp(9)); // Nine
+switchItUp(0); // Zero
+switchItUp(1); // One
+switchItUp(2); // Two
+switchItUp(3); // Three
+switchItUp(4); // Four
+switchItUp(5); // Five
+switchItUp(6); // Six
+switchItUp(7); // Seven
+switchItUp(8); // Eight
+switchItUp(9); // Nine
